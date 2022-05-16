4db84225f5d5f366384e3cedacdb3603a489d15a
Lilianascimento <102737710+Lilianascimento@users.noreply.github.com>
2022-05-16 10:43:05 -0300

    Add files via upload

---
+++ lacos.js
@@ -0,0 +1,22 @@
+/*let x = 1
+while (x <= 100){
+    console.log(x)
+    x = x + 1
+}*/
+
+/*let x =50
+while (x <= 100){
+    console.log(x)
+    x = x + 1
+}*/
+/*let x = 10
+while (x >= 0){
+    console.log(x)
+    x = x - 1
+}*/
+
+let x =0
+while (x <= 20){
+    console.log(x)
+    x = x + 2
+}
